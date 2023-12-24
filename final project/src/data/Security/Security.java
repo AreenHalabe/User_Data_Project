6a6c6987d0723e6ab443be8002bd859c3cf9f122
AreenHalabe <93481480+AreenHalabe@users.noreply.github.com>
2023-12-24 02:22:10 +0200

    Feactch_and_stor_data_from_services

---
+++ final project/src/data/Security/Security.java
@@ -0,0 +1,39 @@
+package data.Security;
+
+import data.Userdata.UserData;
+import iam.IUserService;
+import iam.UserType;
+
+public class Security implements SecurityService{
+
+    private UserType Usertype;
+
+
+    @Override
+    public UserType getUsertype() {
+        return Usertype;
+    }
+
+    @Override
+    public boolean cheack(String name, String password) {
+        var ob = UserData.getUserService().getUser(name);
+
+        if (ob == null) {
+            throw new NullPointerException("User not found for name: " + name);
+
+        }
+
+        else{
+            if (name.equals(ob.getUserName()) && password.equals(ob.getPassword())) {
+                Usertype = ob.getUserType();
+                return true;
+            }
+
+            else{
+                return false;
+            }
+
+        }
+
+    }
+}
--- final project/src/data/Userdata/UserData.java
+++ final project/src/data/Userdata/UserData.java
@@ -1,21 +1,77 @@
 package data.Userdata;
 
 import activity.IUserActivityService;
+import data.Api.Controller;
+import data.Api.ControllerFactory;
+import data.FileStorag.StoregeService;
+import data.FileStorag.TextFile;
+import data.Security.Security;
+import data.Security.SecurityService;
 import iam.IUserService;
 import iam.UserProfile;
 import payment.IPayment;
 import posts.IPostService;
 
-public abstract class UserData {
-    private IUserActivityService iUserActivityService;
-    private IUserService userService;
-    private IPayment iPayment;
-    private IPostService iPostService;
+import javax.naming.NameNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class UserData {
+    private  static IUserActivityService iUserActivityService;
+    private static IUserService userService;
+    private static IPayment iPayment;
+    private static IPostService iPostService;
+    private static SecurityService securityService ;
+
+    private StoregeService storegeService;
+
+    private static List<Controller> Controllers;
     public UserData(IUserActivityService iUserActivityService , IUserService iUserService , IPayment iPayment , IPostService iPostService){
         this.iUserActivityService = iUserActivityService;
         this.userService=iUserService;
         this.iPayment=iPayment;
         this.iPostService=iPostService;
+        securityService = new Security();
+        Controllers = new ArrayList<>();
     }
+
+    public static IUserActivityService getiUserActivityService() {
+        return iUserActivityService;
+    }
+
+    public static IPayment getiPayment() {
+        return iPayment;
+    }
+
+    public static IPostService getiPostService() {
+        return iPostService;
+    }
+
+
+    public static IUserService getUserService(){
+        return userService;
+    }
+
+
+    public void getData(String name , String password)  {
+       boolean UserExist= securityService.cheack(name , password);
+       if(UserExist){
+           storegeService= new TextFile(name+".txt");
+           Controllers = ControllerFactory.CreateController(securityService.getUsertype());
+           FetchData(Controllers , name , storegeService);
+           Controllers.clear();
+       }
+       else{
+           System.out.println("invalid password for this user");
+       }
+    }
+
+
+    private void FetchData(List<Controller> Controllers , String name , StoregeService storegeService){
+        for (Controller controller : Controllers) {
+            controller.getData(name, storegeService);
+        }
+    }
+
+
 }
